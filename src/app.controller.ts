aaedbcbdf5b5d5e0857f9982548c06b43b72b12c
Dmitry Kazantsev <dk@roboticket.com>
2024-07-16 01:17:39 +0200

    feat: origin cors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('hc')
+  healthCheck(): HttpException {
+    return new HttpException('OK', HttpStatus.OK);
   }
 }
