b5433d7db14fc86db58af37be6a2f6196ed91269
Dmitry Kazantsev <dk@roboticket.com>
2024-07-16 11:41:54 +0200

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.enableCors({
-    origin: '*',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
