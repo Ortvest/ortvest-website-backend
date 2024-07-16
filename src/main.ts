f9df26c001a091486864eead2d30ee117cb6fb16
Dmitry Kazantsev <dk@roboticket.com>
2024-07-16 11:09:51 +0200

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,24 @@ async function bootstrap() {
   };
 
   app.enableCors(corsOptions);
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
   await app.listen(8080);
 }
 bootstrap();
