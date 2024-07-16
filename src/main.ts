140b3fc8895473efe4e5478e89ef7543aaab10e3
Dmitry Kazantsev <dk@roboticket.com>
2024-07-16 13:51:38 +0200

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -5,28 +5,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use((req, res, next) => {
-    const corsWhitelist = [
-      'https://ortvest-admin.vercel.app/',
-      'http://localhost:5174/',
-    ];
-    if (corsWhitelist.includes(req.headers.origin)) {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-      res.header('Access-Control-Allow-Credentials', 'true');
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept',
-      );
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept',
-      );
-      res.header('Access-Control-Allow-Methods', [
-        'PUT',
-        'DELETE',
-        'POST',
-        'GET',
-      ]);
-    }
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    res.header('Access-Control-Allow-Methods', [
+      'PUT',
+      'DELETE',
+      'POST',
+      'GET',
+    ]);
     next();
   });
   await app.listen(8080);
