7be83e11b9f5090e9a5cd226fbe7c8bd70d02964
Dmitry Kazantsev <dk@roboticket.com>
2024-07-16 11:13:53 +0200

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.enableCors({
     origin: true,
