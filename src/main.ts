2c4a5050afd075e53777794510c14cb09fd6613c
Dmitry Kazantsev <dk@roboticket.com>
2024-07-16 10:55:09 +0200

    feat: new cors options

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
 
+  const corsOptions: CorsOptions = {
+    origin: ['https://ortvest-admin.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(8080);
 }
 bootstrap();
