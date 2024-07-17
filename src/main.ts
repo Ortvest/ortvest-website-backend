a611917fa13c3b037afd34fbb8ba747362fc337f
Dmitry Kazantsev <dk@roboticket.com>
2024-07-17 13:27:09 +0200

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({
-    origin: ['https://ortvest-admin.vercel.app', 'https://www.ortvest.com/'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-      'Authorization',
-      'Content-Type',
-      'Access-Control-Allow-Origin',
-      'Mode',
-      'Access-Control-Allow-Credentials',
-      'allow-control-allow-origin',
-    ],
-    credentials: true,
-  });
+  //   app.enableCors({
+  //     origin: ['https://ortvest-admin.vercel.app', 'https://www.ortvest.com/'],
+  //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  //     allowedHeaders: [
+  //       'Authorization',
+  //       'Content-Type',
+  //       'Access-Control-Allow-Origin',
+  //       'Mode',
+  //       'Access-Control-Allow-Credentials',
+  //       'allow-control-allow-origin',
+  //     ],
+  //     credentials: true,
+  //   });
 
+  app.use(
+    cors({
+      credentials: true,
+      origin: ['https://ortvest-admin.vercel.app', 'https://www.ortvest.com/'],
+    }),
+  );
   app.use((req, res, next) => {
     const corsWhitelist = [
       'https://ortvest-admin.vercel.app',
