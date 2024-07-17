10604f97dbc425e023807983b9542684f0caca1a
Dmitry Kazantsev <dk@roboticket.com>
2024-07-17 13:31:43 +0200

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -6,51 +6,20 @@ import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  //   app.enableCors({
-  //     origin: ['https://ortvest-admin.vercel.app', 'https://www.ortvest.com/'],
-  //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  //     allowedHeaders: [
-  //       'Authorization',
-  //       'Content-Type',
-  //       'Access-Control-Allow-Origin',
-  //       'Mode',
-  //       'Access-Control-Allow-Credentials',
-  //       'allow-control-allow-origin',
-  //     ],
-  //     credentials: true,
-  //   });
-
-  app.use(
-    cors({
-      credentials: true,
-      origin: ['https://ortvest-admin.vercel.app', 'https://www.ortvest.com/'],
-    }),
-  );
-  app.use((req, res, next) => {
-    const corsWhitelist = [
-      'https://ortvest-admin.vercel.app',
-      'https://www.ortvest.com/',
-    ];
-    if (corsWhitelist.includes(req.headers.origin)) {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-      res.header('Access-Control-Allow-Credentials', 'true');
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept',
-      );
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept',
-      );
-      res.header('Access-Control-Allow-Methods', [
-        'PUT',
-        'DELETE',
-        'POST',
-        'GET',
-      ]);
-    }
-    next();
+  app.enableCors({
+    origin: ['https://ortvest-admin.vercel.app', 'https://www.ortvest.com/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    // allowedHeaders: [
+    //   'Authorization',
+    //   'Content-Type',
+    //   'Access-Control-Allow-Origin',
+    //   'Mode',
+    //   'Access-Control-Allow-Credentials',
+    //   'allow-control-allow-origin',
+    // ],
+    credentials: true,
   });
+
   await app.listen(8080);
 }
 bootstrap();
