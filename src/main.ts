bccb8a3856cd69bc656edfee3f0e5ac6d52ee7c3
Dmitry Kazantsev <dk@roboticket.com>
2024-07-20 11:08:48 +0200

    feat: updated cors middleware

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { NextFunction, Response, Request } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({
-    origin: '*',
-    credentials: true,
+
+  const allowedOrigins = [
+    'https://ortvest-admin.vercel.app',
+    'https://www.ortvest.com',
+  ];
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const origin = req.headers.origin as string;
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, mode',
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(204);
+    } else {
+      next();
+    }
   });
   await app.listen(8080);
 }
