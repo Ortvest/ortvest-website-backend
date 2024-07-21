3a5a551a0fb6d205f4da19be5da6809837fb8329
Dmitry Kazantsev <dk@roboticket.com>
2024-07-22 01:10:03 +0200

    feat: moved middleware to separeted func

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { NextFunction, Response, Request } from 'express';
 
-import { corsMiddleware } from 'src/shared/middlewares/cors.middleware';
-
+const ortvestWebsiteUrl = process.env.ORTVEST_WEBSITE_URL;
+const ortvestAdminUrl = process.env.ORTVEST_ADMIN_URL;
+const localServer = process.env.LOCAL_FE;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(corsMiddleware);
+  const allowedOrigins = [ortvestWebsiteUrl, ortvestAdminUrl, localServer];
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const origin = req.headers.origin as string;
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, mode',
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(204);
+    } else {
+      next();
+    }
+  });
   await app.listen(8080);
 }
 bootstrap();
